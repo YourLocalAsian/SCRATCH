919df278e8755839604f12279e1526a71c1d9a17
Luke Ambray <lukeambray@knights.ucf.edu>
2022-09-21 22:24:56 -0400

    Changed pin mapping

--- Cue Stick/Individual Components/Button Code/buttons_low_power/buttons_low_power.ino
+++ Cue Stick/Individual Components/Button Code/buttons_low_power/buttons_low_power.ino
@@ -1,5 +1,12 @@
 #include <string>
+#include "driver/rtc_io.h"
 
+// Definitions for low-power modes
+#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+#define BUTTON_PIN_BITMASK 0xE007000
+
+// Button pin definitions
 #define UP_PIN 14
 #define DOWN_PIN 12
 #define LEFT_PIN 13
@@ -33,6 +40,9 @@ void ButtonA_ISR();
 void ButtonB_ISR();
 void (* buttonISR[])() = {ButtonUp_ISR, ButtonDown_ISR, ButtonLeft_ISR, ButtonRight_ISR, ButtonA_ISR, ButtonB_ISR};
 
+void print_wake_up();
+void print_wakeup_reason();
+
 
 void setup() {
   Serial.begin(9600);
@@ -43,14 +53,19 @@ void setup() {
     pinMode(buttonArray[i].pin_, INPUT_PULLUP); // set pin mode
     attachInterrupt(buttonArray[i].pin_, buttonISR[i], FALLING);
   }
-  
+
+  esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,ESP_EXT1_WAKEUP_ANY_HIGH);
 }
 
 void loop() {
+  print_wakeup_reason();
+  print_wake_up();
+  esp_sleep_enable_ext1_wakeup(BUTTON_PIN_BITMASK,ESP_EXT1_WAKEUP_ANY_HIGH);
+  
   // Check if buttons are pressed
   uint8_t buttonsPressed = 0;
 
-  for (int i = 0; i < 20; ++i)
+  for (int i = 0; i < 10; ++i)
   {
     if (buttonUp.pressed_) 
     {
@@ -102,6 +117,9 @@ void loop() {
 
   // Reset number of buttons pressed
   buttonsPressed = 0;
+
+  esp_deep_sleep_start();
+  Serial.print("KENNY IS ALIVE");
 }
 
 
@@ -164,5 +182,26 @@ void ButtonB_ISR()
   {
        buttonB.pressed_ = true;
        buttonB.last_button_time_ = buttonB.button_time_;
+  }
+}
+
+void print_wake_up(){
+  int GPIO_reason = esp_sleep_get_ext1_wakeup_status();
+  Serial.print("GPIO that triggered the wake up: GPIO ");
+  Serial.println((log(GPIO_reason))/log(2), 0);
+}
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason){
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
   }
 }
