c6637a37eb3ae6dbbe0681be5e7e13e2b74527dc
Luke Ambray <lukeambray@knights.ucf.edu>
2023-01-11 12:54:33 -0500

    Removed BLE code from main file, added reference

--- Cue Stick/Individual Components/IMU/Simulation/ported_simulation_BT/ported_simulation_BT.ino
+++ Cue Stick/Individual Components/IMU/Simulation/ported_simulation_BT/ported_simulation_BT.ino
@@ -6,11 +6,7 @@
 #include <Vector.h>
 
 #include "buttons.h"
-
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
+#include "BLE_server.h"
 
 #define BNO055_SAMPLERATE_DELAY_MS 100
 #define MIN_ACCELERATION -1000
@@ -74,25 +70,6 @@ void configurePrint();
 void basicPrint(imu::Vector<3> eulerVector);
 void graphPrint(imu::Vector<3> eulerVector);
 
-// * BLE
-#define bleServerName "CUE_ESP32" // BLE server name
-bool deviceConnected = false;
-#define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59" // https://www.uuidgenerator.net/
-
-// Cue Acceleration Characteristic and Descriptor
-BLECharacteristic cueAccelerationCharacteristics("ca73b3ba-39f6-4ab3-91ae-186dc9577d99", BLECharacteristic::PROPERTY_NOTIFY);
-BLEDescriptor cueAccelerationDescriptor(BLEUUID((uint16_t)0x2903));
-
-// Setup callbacks onConnect and onDisconnect
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-        deviceConnected = true;
-    };
-    void onDisconnect(BLEServer* pServer) {
-        deviceConnected = false;
-    }
-};
-
 void initBNO() {
     if(!bno.begin()) {
         /* There was a problem detecting the BNO055 ... check your connections */
@@ -121,32 +98,8 @@ void setup() {
     configureOperation();
     configurePrint();
     floodStationary(-100);
-  
-    // * Setup Bluetooth
-    // Create the BLE Device
-    BLEDevice::init(bleServerName);
-
-    // Create the BLE Server
-    BLEServer *pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks());
-
-    // Create the BLE Service
-    BLEService *cueService = pServer->createService(SERVICE_UUID);
     
-    // Create BLE Characteristics and Create a BLE Descriptor
-    // Acceleration
-    cueService->addCharacteristic(&cueAccelerationCharacteristics);
-    cueAccelerationDescriptor.setValue("IMU acceleration");
-    cueAccelerationCharacteristics.addDescriptor(new BLE2902());
-    
-    // Start the service
-    cueService->start();
-
-    // Start advertising
-    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(SERVICE_UUID);
-    pServer->getAdvertising()->start();
-    Serial.println("Waiting a client connection to notify...");
+    setupBLE(); // Initialize BLE connection
 }
 
 // Main functionality of simulation
@@ -535,7 +488,10 @@ void basicPrint(imu::Vector<3> eulerVector) {
     if (printShotReady) Serial.printf("Shot Ready: %s\n", shotReady ? "True" : "False");
     if (printShotAttempt) Serial.printf("Shot Attempt: %s\n", shotAttempt ? "True" : "False");
     if (printVectorSize) Serial.printf("Vector Size: %d\n", accelerationValues.size());
-    if (printAcceleration) Serial.printf("Acceleration: %.3f\n", avgAcceleration);
+    if (printAcceleration) {
+        Serial.printf("Acceleration: %.3f\n", avgAcceleration);
+        updateAccelerationCharacteristics(avgAcceleration);
+    }
     if (printRoll) Serial.printf("Roll: %.3f\n", -180/M_PI * (double) eulerVector.z() - calibrationData[1]);
     if (printPitch) Serial.printf("Pitch: %.3f\n", 180/M_PI * (double) eulerVector.y() - calibrationData[2]);
     if (printYaw) Serial.printf("Yaw: %.3f\n", -(180/M_PI * (double) eulerVector.x() - calibrationData[3]));
