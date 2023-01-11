92be1e13afa2618e94851abea5f507f388182e28
Luke Ambray <lukeambray@knights.ucf.edu>
2023-01-10 22:23:43 -0500

    Added button history code, not tested

---
+++ Cue Stick/Cue Stick Application/cue_stick_app/cue_stick_app.ino
@@ -0,0 +1,110 @@
+// Cue Stick Application
+
+#include <stdint.h>
+#include "shot_fms.h"
+#include "buttons.h"
+
+#define STANDBY_MODE 0
+#define SHOT_MODE 1
+#define DEBUG_MODE 2
+#define KONAMI_CODE 87
+
+// * Global Variables 
+int cueStickState;
+int buttonHistoryArray[10];
+int buttonHistoryIndex;
+bool konamiCodeInput;
+
+// Main Functions
+void connectToCCU(); // TODO
+void storeButtonHistory();
+void checkForKonami();
+
+void setup() {
+    // Configure serial (console)
+    Serial.begin(115200);
+
+    // Configure cue stick hardware
+    setupHardware(); // * call modified version of ported_simulation's setup
+
+     // Setup Bluetooth connection to CCU
+    connectToCCU(); // TODO: figure out with Mena
+
+    // Set stick to standby mode
+    cueStickState = STANDBY_MODE;
+}
+
+void loop() {
+    switch(cueStickState) {
+        case STANDBY_MODE: {
+            int numOfButtonsPressed = readAllButtons(); // wait for button input
+            // * Printing happens in readAllButtons()
+            storeButtonHistory(); // store button inputs for checking for input codes
+            checkForKonami();
+        } 
+        
+        case SHOT_MODE: {
+            fmsLoop();
+        } 
+        
+        case KONAMI_CODE: {
+            Serial.println("You typed the Konami code. You get 30 lives");
+            cueStickState = STANDBY_MODE;
+        }
+
+        default: {
+
+        }
+    }   
+}
+
+// TODO: Figure out how to connect cue stick to CCU
+void connectToCCU() { 
+
+}
+
+void storeButtonHistory() {
+    for (int index = 0; index < 6; index++) { // Iterate through pressedArray 
+        if (pressedArray[index]) { // if button is pressed
+            buttonHistoryArray[buttonHistoryIndex] = index; // Store button at current buttonHistoryIndex in buttonHistoryArray
+            buttonHistoryIndex = (buttonHistoryIndex + 1) % 10; // buttonHistoryIndex index
+        }
+    }
+}
+void checkForKonami() { // check for Konami code in reverse
+    bool possibleCode = true;
+    int tempIndex = (buttonHistoryIndex + 1) % 10;
+    
+    if (buttonHistoryArray[tempIndex] == UP) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    if (buttonHistoryArray[tempIndex] == UP && possibleCode) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    if (buttonHistoryArray[tempIndex] == DOWN && possibleCode) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    if (buttonHistoryArray[tempIndex] == DOWN && possibleCode) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    if (buttonHistoryArray[tempIndex] == LEFT && possibleCode) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    if (buttonHistoryArray[tempIndex] == RIGHT && possibleCode) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    if (buttonHistoryArray[tempIndex] == LEFT && possibleCode) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    if (buttonHistoryArray[tempIndex] == RIGHT && possibleCode) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    if (buttonHistoryArray[tempIndex] == B && possibleCode) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    if (buttonHistoryArray[tempIndex] == A && possibleCode) {
+        tempIndex = (tempIndex + 1) % 10;
+    }
+    
+    if (possibleCode) cueStickState = KONAMI_CODE;
+}
---
+++ Cue Stick/Cue Stick Application/cue_stick_app/shot_fms.h
@@ -0,0 +1,511 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <typeinfo>
+#include <Vector.h>
+
+#include "buttons.h"
+
+#define BNO055_SAMPLERATE_DELAY_MS 100
+#define MIN_ACCELERATION -1000
+#define MAX_ACCELERATION 1000
+#define ACC_MARGIN 1
+const int ELEMENT_COUNT_MAX = 30;
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
+
+// Global storage variables
+double storage_array[ELEMENT_COUNT_MAX];
+Vector<double> accelerationValues(storage_array);
+double stationaryValues[10];
+double calibrationData[4];
+int stationaryIndex = 0;
+double oldAcceleration;
+double avgAcceleration;
+double minima;
+int mapped;
+
+// Global status variables
+int zeroCount;
+bool zeroedOut = false;
+bool isStationary;
+bool shotReady;
+double accelerationBase;
+double globalMinima = 0;
+bool shotAttempt;
+int fmsState = 0;
+
+// Operation mode variables
+bool speedCheckMode;
+
+// Print variables
+bool printGraphForm;
+bool printFmsState;
+bool printStationary;
+bool printShotReady;
+bool printShotAttempt;
+bool printVectorSize;
+bool printMappedValue;
+bool printAcceleration;
+bool printRoll;
+bool printPitch;
+bool printYaw;
+
+// Mapping arrays
+const int mapArray[7] = {-1, -3, -5, -7, -9, -11, -13};
+String ballSpeed[] = {"SOFT_TOUCH", "SLOW", "MEDIUM", 
+                            "FAST", "POWER", "BREAK", "POWER_BREAK"};
+String stateMap[] = {"NOT_READY", "READY", "WAITING", "TAKING_SHOT", "SHOT_TAKEN"};
+
+// Supplementary functions
+void checkStationary(double avgAcceleration);
+double findGlobalMinima(Vector<double> accelerationValues);
+int mapAcceleration(double acceleration);
+void floodStationary();
+void floodStationary(int floodValue);
+void zeroOut();
+void configurePrint();
+void basicPrint(imu::Vector<3> eulerVector);
+void graphPrint(imu::Vector<3> eulerVector);
+
+// Configures the simulation
+void setupHardware() {
+    Serial.println("Cue Stick IMU Test\n");
+
+    // Initialise the sensor
+    if(!bno.begin()) {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while(1);
+    }
+
+    delay(1000);
+    bno.setExtCrystalUse(true);
+    Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
+    setupButtons();
+
+    delay(5);
+    zeroOut(); // Get baseline orientation of stick
+    configureOperation();
+    configurePrint();
+    floodStationary(-100);
+}
+
+// Main functionality of simulation
+void fmsLoop() {
+    // Get accleration and orientation vectors
+    imu::Vector<3> accelerationVector = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    imu::Quaternion quaternionVector = bno.getQuat();
+    quaternionVector.normalize();
+    imu::Vector<3> eulerVector = quaternionVector.toEuler();
+
+    if (!zeroedOut) { // If not zeroedOut
+        if (zeroCount < 20) { // If zeroCount < 20
+            double roll = -180/M_PI * eulerVector.z();
+            double pitch = 180/M_PI * eulerVector.y();
+            double yaw = 180/M_PI * eulerVector.x();
+            Serial.printf("Roll = %lf\n", roll);
+            if (!isnan(roll)) {
+                calibrationData[0] += accelerationVector.x();
+                calibrationData[1] += roll;
+                calibrationData[2] += pitch;
+                calibrationData[3] += yaw;
+                Serial.printf("zeroCount = %d\n", zeroCount);
+                zeroCount++;
+            }
+        }
+
+        if (zeroCount == 20) { // If zeroCount == 20
+            for (int i = 0; i < 4; i++) calibrationData[i] /= 20; // Divide calibrationData values by 20
+            Serial.printf("Roll: %lf, Pitch: %lf, Yaw: %lf\n", calibrationData[1], calibrationData[2], calibrationData[3]);
+            zeroedOut = true; // Set zeroedOut to true
+        }
+    }
+   
+    if (zeroedOut) { // If zeroedOut
+        avgAcceleration = ((accelerationVector.x() - calibrationData[0]) + oldAcceleration) / 2; //  Smooth accleration value
+
+        checkStationary(avgAcceleration); // Check if stick is stationary
+
+        // FSM 
+        if (!isStationary && !shotReady) { // State 0: Not stationary, not ready to take shot
+            fmsState = 0;
+        } else if (isStationary && !shotReady) { // State 1: Stationary, not ready to take shot -> ready to take shot
+            accelerationBase = avgAcceleration; // Set base line acceleration for shot
+            floodStationary(avgAcceleration); // Flood array
+            globalMinima = avgAcceleration;
+            shotReady = true;
+            fmsState = 1;
+        } else if (isStationary && shotReady && accelerationValues.empty()) { // State 2: Stationary, ready to take shot, hasn't moved -> wait for shot
+            fmsState = 2;
+        } else if (!isStationary && shotReady) { // State 3: Not stationary, ready to take shot (taking shot) -> self
+            double pushedValue = avgAcceleration - accelerationBase;
+            accelerationValues.push_back(pushedValue);
+            fmsState = 3;
+        } else if (isStationary && shotReady && !accelerationValues.empty()) {  // State 4: Stationary, ready to take shot (done taking shot) -> reset
+            for (int i = 0; i < accelerationValues.size(); i++) accelerationValues.pop_back();
+            floodStationary();
+            shotAttempt = true;
+            shotReady = false;
+            fmsState = 4;
+        }
+    }
+
+    // Print data
+    if (printGraphForm)
+        graphPrint(eulerVector);
+    else basicPrint(eulerVector);
+    
+    if (shotAttempt) {
+        minima = findGlobalMinima(accelerationValues);
+        accelerationValues.clear();
+        mapped = mapAcceleration(minima);
+        if (printMappedValue) {
+            Serial.printf("Minima: %.3f, Mapped: %s\n\n", minima, ballSpeed[mapped]);
+            if (speedCheckMode) while (!checkButton(buttonA)) {} // blocking statement
+        }
+        shotAttempt = false;
+    }
+
+    oldAcceleration = avgAcceleration;
+
+    delay(BNO055_SAMPLERATE_DELAY_MS);
+}
+
+// Finds the strongest deceleration
+double findGlobalMinima(Vector<double> accelerationValues) {
+    double minimum = MAX_ACCELERATION;
+    for (double s : accelerationValues) {
+        minimum = (s < minimum) ? s : minimum;
+    }
+
+    return minimum;
+}
+
+// Maps acceleration to meaningful value
+int mapAcceleration(double acceleration) {
+    int mapped = 0;
+    for (int i = 0; i < 7; i++)
+        if ((int) acceleration <= mapArray[i]) mapped = i;
+    
+    return mapped;
+}
+
+// Checks if cue stick is stationary
+void checkStationary(double avgAcceleration) {
+    stationaryValues[stationaryIndex] = avgAcceleration; // Store queue of 10 acceleration values
+    double minAcc = MAX_ACCELERATION;
+    double maxAcc = MIN_ACCELERATION;
+    
+    for (int i = 0; i < 10; i++){
+        if (stationaryValues[i] < minAcc) minAcc = stationaryValues[i];
+        if (stationaryValues[i] > maxAcc) maxAcc = stationaryValues[i];
+    }
+
+    isStationary = (maxAcc - minAcc <= ACC_MARGIN) ? true : false; // if the min and max differenitate less than 0.5, set stationary to true
+    stationaryIndex = (stationaryIndex + 1) % 10; // wrap around
+}
+
+// Resets stationary value array with non-stationary values
+void floodStationary() {
+    for (int i = 0; i < 10; i++) stationaryValues[i] = (i % 2 == 0) ? MIN_ACCELERATION : MAX_ACCELERATION;    
+}
+
+// Resets stationary value array
+void floodStationary(int floodValue) {
+    for (int i = 0; i < 10; i++) stationaryValues[i] = floodValue;    
+}
+
+// Level IMU out w.r.t table
+void zeroOut() {
+    for (int i = 0; i < 4; i++) 
+        calibrationData[i] = 0;
+    
+    for (int i = 0; i < 20; i++) {
+        imu::Vector<3> accelerationVector = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+        imu::Quaternion quaternionVector = bno.getQuat();
+        quaternionVector.normalize();
+        imu::Vector<3> eulerVector = quaternionVector.toEuler();
+
+        double roll = -180/M_PI * eulerVector.z();
+        if(isnan(roll)) 
+            i--;
+        else {
+            calibrationData[0] += accelerationVector.x();
+            calibrationData[1] += 180/M_PI * eulerVector.z();
+            calibrationData[2] += 180/M_PI * eulerVector.y();
+            calibrationData[3] += 180/M_PI * eulerVector.x();
+        }
+        delay(BNO055_SAMPLERATE_DELAY_MS);
+    }
+
+    for (int i = 0; i < 4; i++) {
+        calibrationData[i] /= 20;
+        Serial.printf("Offset #%d: %lf\n", i, calibrationData[i]);
+    }
+
+    zeroedOut = true;
+    isStationary = false;
+    shotReady = false;
+    shotAttempt = false;
+    oldAcceleration = calibrationData[0];
+}
+
+// Configure cue stick operation
+void configureOperation() {
+    uint8_t valuesConfigured = 0;
+
+    if (valuesConfigured == 0) Serial.print("Speed check mode?: ");
+    while (valuesConfigured == 0) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            speedCheckMode = true;
+            valuesConfigured = 8;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            speedCheckMode = false;
+            valuesConfigured++;
+        }
+    }
+}
+
+// Configure which values are printed
+void configurePrint() {
+    uint8_t valuesConfigured = 0;
+
+    // Print in graph form
+    Serial.print("Print in graph form: ");
+    while (valuesConfigured == 0) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printGraphForm = true;
+            printFmsState = true;
+            printAcceleration = true;
+            printRoll = true;
+            printPitch = true;
+            printYaw = true;
+            valuesConfigured = 8;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printGraphForm = false;
+            valuesConfigured++;
+        }
+    }
+
+    // Use default configuration
+    if (valuesConfigured == 1) Serial.print("Use default (all printed): ");
+    while (valuesConfigured == 1) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printFmsState = true;
+            printStationary = true;
+            printShotReady = true;
+            printShotAttempt = true;
+            printVectorSize = true;
+            printMappedValue = true;
+            printAcceleration = true;
+            printRoll = true;
+            printPitch = true;
+            printYaw = true;
+            valuesConfigured = 20;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            valuesConfigured++;
+        }
+    }
+
+    // Set printFmsState
+    if (valuesConfigured == 2) Serial.print("Print State?: ");
+    while (valuesConfigured == 2) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printFmsState = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printFmsState = false;
+            valuesConfigured++;
+        }
+    }
+    
+    // Set printStationary
+    if (valuesConfigured == 3) Serial.print("Print isStationary?: ");
+    while (valuesConfigured == 3) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printStationary = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printStationary = false;
+            valuesConfigured++;
+        }
+    }
+    
+    // Set printShotReady
+    if (valuesConfigured == 4) Serial.print("Print shotReady?: ");
+    while (valuesConfigured == 4) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printShotReady = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printShotReady = false;
+            valuesConfigured++;
+        }
+    }
+    
+    // Set printShotAttempt
+    if (valuesConfigured == 5) Serial.print("Print FMS States?: ");
+    while (valuesConfigured == 5) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printShotAttempt = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printShotAttempt = false;
+            valuesConfigured++;
+        }
+    }
+    
+    // Set printVectorSize
+    if (valuesConfigured == 6) Serial.print("Print vector size?: ");
+    while (valuesConfigured == 6) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printVectorSize = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printVectorSize = false;
+            valuesConfigured++;
+        }    
+    }
+
+    // Set printMappedValue
+    if (valuesConfigured == 7) Serial.print("Print mapped value?: ");
+    while (valuesConfigured == 7) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printMappedValue = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printMappedValue = false;
+            valuesConfigured++;
+        }    
+    }
+
+    // Set printAcceleration
+    if (valuesConfigured == 8) Serial.print("Print acceleration?: ");
+    while (valuesConfigured == 8) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printAcceleration = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printAcceleration = false;
+            valuesConfigured++;
+        }
+    }
+
+    // Set printRoll
+    if (valuesConfigured == 9) Serial.print("Print roll?: ");
+    while (valuesConfigured == 9) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printRoll = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printRoll = false;
+            valuesConfigured++;
+        }    
+    }
+
+    // Set printPitch
+    if (valuesConfigured == 10) Serial.print("Print pitch?: ");
+    while (valuesConfigured == 10) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printPitch = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printPitch = false;
+            valuesConfigured++;
+        }   
+    }
+
+    // Set printYaw
+    if (valuesConfigured == 11) Serial.print("Print yaw?: ");
+    while (valuesConfigured == 11) {
+        if (checkButton(buttonA)) {
+            Serial.println("Y");
+            printYaw = true;
+            valuesConfigured++;
+        }
+        if (checkButton(buttonB)) {
+            Serial.println("N");
+            printYaw = false;
+            valuesConfigured++;
+        }    
+    }
+
+    Serial.println();
+}
+
+// Print for monitor
+void basicPrint(imu::Vector<3> eulerVector) {
+    if (printFmsState) Serial.printf("State: %s\n", stateMap[fmsState]);
+    if (printStationary) Serial.printf("Stationary: %s\n", isStationary ? "True" : "False");
+    if (printShotReady) Serial.printf("Shot Ready: %s\n", shotReady ? "True" : "False");
+    if (printShotAttempt) Serial.printf("Shot Attempt: %s\n", shotAttempt ? "True" : "False");
+    if (printVectorSize) Serial.printf("Vector Size: %d\n", accelerationValues.size());
+    if (printAcceleration) Serial.printf("Acceleration: %.3f\n", avgAcceleration);
+    if (printRoll) Serial.printf("Roll: %.3f\n", -180/M_PI * (double) eulerVector.z() - calibrationData[1]);
+    if (printPitch) Serial.printf("Pitch: %.3f\n", 180/M_PI * (double) eulerVector.y() - calibrationData[2]);
+    if (printYaw) Serial.printf("Yaw: %.3f\n", -(180/M_PI * (double) eulerVector.x() - calibrationData[3]));
+    Serial.println();
+}
+
+// Print for plotter
+void graphPrint(imu::Vector<3> eulerVector) {
+    if (printFmsState) {
+        Serial.print("State: ");
+        Serial.print(fmsState);
+    }
+    if (printAcceleration) {
+        Serial.print(" Acceleration: ");
+        Serial.print(avgAcceleration, 3);
+    }
+    if (printRoll) {
+        Serial.print(" Roll: ");
+        Serial.print(-180/M_PI * (double) eulerVector.z() - calibrationData[1], 3);
+    }
+    if (printPitch) {
+        Serial.print(" Pitch: ");
+        Serial.print(180/M_PI * (double) eulerVector.y() - calibrationData[2], 3);
+    }
+    if (printYaw) {
+        Serial.print(" Yaw: ");
+        Serial.print(-(180/M_PI * (double) eulerVector.x() - calibrationData[3]), 3);
+    }
+
+    Serial.println("\t\t");
+}
